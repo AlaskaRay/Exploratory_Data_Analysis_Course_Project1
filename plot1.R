cd71b2d16dfbcadeb83a86d01123cee175d4db28
AlaskaRay <forest.ray@zoho.com>
2015-06-07 08:54:12 -0700

    plot1.R
    Creates histogram of global active power for 2 day period.

---
+++ plot1.R
@@ -0,0 +1,12 @@
+## Exploratory Data Analysis Course Project 1, plot 1
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE)
+
+# Subset on dates in the range 2007-02-01 and 2007-02-02
+subData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+# Plot histogram of global active power (plot1)
+png("plot1.png", width=480, height=480)
+hist(as.numeric(subData$Global_active_power), col="red", xlab="Global Active Power (kilowatts)", 
+     ylab="Frequency", main="Global Active Power")
+dev.off()
+
